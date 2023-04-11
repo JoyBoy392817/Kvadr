bccb8853343c3e418c358b7026f5ae052825edde
daniel.prchlik <daniel.prchlik@VT3-24>
2023-04-11 12:26:04 +0200

    Přidejte soubory projektu.

---
+++ testovac/Program.cs
@@ -0,0 +1,7 @@
+﻿using testovac;
+
+Kvadr testObjekt = new Kvadr(5,15,15);
+
+Console.WriteLine(testObjekt.Objem());
+Console.WriteLine(testObjekt.Povrch());
+Console.WriteLine(testObjekt.Uhlopricky);
---
+++ testovac/kvadr.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testovac
+{
+    internal class Kvadr
+    {
+        public double StranaA { get; set; }
+        public double StranaB { get; set; }
+        public double StranaC { get; set; }
+        public Kvadr(double a, double b, double c)
+        {
+            StranaA = a;
+            StranaB = b;
+            StranaC = c;
+        }
+        public double Objem()
+        {
+            return (StranaC * StranaB * StranaA);
+        }
+        public double Povrch()
+        {
+            return 2 * (StranaA * StranaB + StranaA * StranaC + StranaB * StranaC);
+        }
+        public double Uhlopricky()
+        {
+            return Math.Sqrt(Math.Pow(StranaA, 2)+Math.Pow(StranaB, 2)+Math.Pow(StranaC, 2));
+        }
+        public double Uhlop
+        {
+            return 
+        }
+        //public double Uhlopricak()
+        //{
+        //    //to do 
+        //}
+    }
+}
