c357701a9a3b57db7ad73c19e1e39307158c14e8
JijkaCZ <125461759+JoyBoy392817@users.noreply.github.com>
2023-04-18 09:34:53 +0200

    Add files via upload

---
+++ testovac/testovac/Program.cs
@@ -0,0 +1,10 @@
+﻿using testovac;
+double a = Double.Parse(Console.ReadLine());
+double b = Double.Parse(Console.ReadLine());
+double c = Double.Parse(Console.ReadLine());
+Kvadr testObjekt = new Kvadr(a, b, c);
+
+Console.WriteLine(testObjekt.Objem());
+Console.WriteLine(testObjekt.Povrch());
+Console.WriteLine(testObjekt.Uhlop());
+Console.WriteLine(testObjekt.Uhlopricky());
